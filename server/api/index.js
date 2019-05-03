8fefe236bbc7627fd0a8e8c8bb18a68b9cc6da3c
Conrad <theradistone@gmail.com>
2019-05-03 16:55:09 -0400

    page Api Complete

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,8 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/images', require('./image'))
 router.use('/videos', require('./video'))
+router.use('/pages', require('./page'))
+router.use('/scrapbooks', require('./scrapbook'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/scrapbook.js
@@ -0,0 +1,44 @@
+const router = require('express').Router()
+const { ScrapBook } = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+    try {
+        const userId = req.params.id
+        const scrapbooks = await ScrapBook.findAll({
+            where: { userId }
+        })
+        res.status(200).json(scrapbooks)
+      } catch(err){
+          next(err)
+      }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+      const id = req.params.id
+      const scrapbook = await ScrapBook.findByPk(id)
+
+      res.status(200).json(scrapbook)
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const scrapbook = await Image.findByPk(id)
+
+        const updatedScrapbook = await scrapbook.update(req.body)
+        res.status(200).json(updatedScrapbook)
+    } catch(err) {next(err)}
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        await ScrapBook.destroy({where: {id}})
+        res.status(200)
+    } catch(err) {next(err)}
+})
