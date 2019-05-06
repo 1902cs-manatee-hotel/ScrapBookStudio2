cbcff767e81ceab4bc6791e3d21173c525cd8f38
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-05-06 11:28:37 -0400

    started login page

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
+import {Link} from 'react-router-dom'
 
 /**
  * COMPONENT
@@ -23,6 +24,9 @@ const AuthForm = props => {
             <small>Password</small>
           </label>
           <input name="password" type="password" />
+        </div>
+        <div>
+        <Link to="/signup">Sign Up</Link>
         </div>
         <div>
           <button type="submit">{displayName}</button>
