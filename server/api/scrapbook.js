7bf9e6cd5d2465671dd6d1494b5c711565744375
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-05-07 15:18:21 -0400

    changed scrapbook spelling

--- server/api/scrapbook.js
+++ server/api/scrapbook.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const { ScrapBook } = require('../db/models')
+const { Scrapbook } = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
     try {
         console.log('req.body', req.user)
         const id = req.user.dataValues.id
-        const scrapbooks = await ScrapBook.findAll({
+        const scrapbooks = await Scrapbook.findAll({
             where: { userId: id }
         })
         res.status(200).json(scrapbooks)
@@ -18,7 +18,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
     try {
       const id = req.params.id
-      const scrapbook = await ScrapBook.findByPk(id)
+      const scrapbook = await Scrapbook.findByPk(id)
 
       res.status(200).json(scrapbook)
     } catch (err) {
@@ -28,7 +28,7 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-      const scrapbook = await ScrapBook.create(req.body)
+      const scrapbook = await Scrapbook.create(req.body)
       res.status(200).json(scrapbook)
   } catch(err) {next(err)}
 })
@@ -36,7 +36,7 @@ router.post('/', async (req, res, next) => {
 router.put('/:id', async (req, res, next) => {
     try {
         const id = req.params.id
-        const scrapbook = await ScrapBook.findByPk(id)
+        const scrapbook = await Scrapbook.findByPk(id)
 
         const updatedScrapbook = await scrapbook.update(req.body)
         res.status(200).json(updatedScrapbook)
@@ -46,7 +46,7 @@ router.put('/:id', async (req, res, next) => {
 router.delete('/:id', async (req, res, next) => {
     try {
         const id = req.params.id
-        await ScrapBook.destroy({where: {id}})
+        await Scrapbook.destroy({where: {id}})
         res.status(200)
     } catch(err) {next(err)}
 })
