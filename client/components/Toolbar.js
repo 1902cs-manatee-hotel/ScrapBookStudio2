e427efa0ba4d99c81f88cceb4b4a299bab09f7b4
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-05-07 13:46:44 -0400

    deleted html button

--- client/components/Toolbar.js
+++ client/components/Toolbar.js
@@ -7,8 +7,6 @@ class Toolbar extends Component {
       <div className="box toolbar">
         <h1 className="title">Toolbar</h1>
         <ImageUpload />
-        <button className='button is-primary space' type='submit'>Image</button>
-        <br/>
         <button className='button is-primary space' type='submit'>Text</button>
         <br/>
         <button className='button is-primary space' type='submit'>Background</button>
