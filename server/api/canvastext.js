5964aac35793f02481c3a6436a84155121cf6213
Christopher Najafi <chris.najafi@gmail.com>
2019-05-14 12:47:36 -0400

    created increase and decrease routes

--- server/api/canvastext.js
+++ server/api/canvastext.js
@@ -33,6 +33,26 @@ router.put('/:id', async (req, res, next) => {
     }
 })
 
+router.put('/increase/:id', async (req, res, next) => {
+    try {
+        const canvasText = await CanvasText.findByPk(req.params.id)
+        const updatedCanvasText = await canvasText.update({size: canvasText.size++})
+        res.status(200).json(updatedCanvasText)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.put('/decrease/:id', async (req, res, next) => {
+    try {
+        const canvasText = await CanvasText.findByPk(req.params.id)
+        const updatedCanvasText = await canvasText.update({size: canvasText.size--})
+        res.status(200).json(updatedCanvasText)
+    } catch (error) {
+        next(error)
+    }
+})
+
 router.delete('/:id', async (req, res, next) => {
     try {
         const {id} = req.params
