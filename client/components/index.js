d6735ea8b58912b20e656ebc7d93346e2b5cff93
Conrad <theradistone@gmail.com>
2019-05-14 12:29:54 -0400

    deleted userhome component and spec

--- client/components/index.js
+++ client/components/index.js
@@ -4,7 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as UserHome} from './User-Home'
 export {Login, Signup} from './auth-form'
 export {default as Create} from './Create'
 export {default as ScrapbookSetup} from './ScrapbookSetup'
