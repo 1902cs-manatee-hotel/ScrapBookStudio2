63bdec3cc9d167c88355ba3e64c8cb78f831cfc9
Eric Folks <efolks@live.com>
2019-05-16 11:14:33 -0400

    sign up implemented

--- server/auth/index.js
+++ server/auth/index.js
@@ -21,7 +21,9 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
+    console.log('REQ BODY', req.body)
     const user = await User.create(req.body)
+    console.log('CREATED USER', user)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
